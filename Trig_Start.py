3e476b1e8419062a1f7695ddaf31b5c8a8a64cc0
dcndn4 <stokes@iphouse.com>
2022-08-13 13:12:47 -0500

    start

---
+++ Trig_Start.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 13 13:06:58 2022
+
+@author: CS_Knit_tinK_SC
+"""
+
+#Trig Start
+
+import math
+
+# pyth theorem
+
+# c2 = a2 + b2
+# where c is hypotenuse
+
+#%%
+
+#c**2 = a**2 + b**2
+
+# if c=5 and a = 3, what is b
+
+# 5**2 = 3**2 + b**2
+
+# b**2 = 25 - 9
+#b**2 = 16
+# b=4!
+
+b=math.sqrt(25-9)
