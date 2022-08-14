bb1bf4e75e2b9e8093d7727dc647abadcdeabbf7
dcndn4 <stokes@iphouse.com>
2022-08-14 10:02:04 -0500

    finish 1st chptr

--- Trig_Start.py
+++ Trig_Start.py
@@ -49,3 +49,60 @@ i=math.sqrt(65.27**2-41.955**2)
 
 j=math.sqrt(9-2.9544**2)
 #%%
+
+# converting angle degrees to minutes
+# 1 minute = 1/60 degree
+
+# degrees, minutes, seconds
+
+# in 16.5 degrees
+# 16.5 degrees = 16 degrees, 30 min
+
+# in 22.333 degrees
+i=60*.333
+# .333 = 19.98 minutes
+# 22 degrees 20 minutes
+
+# in 2.22 x 10-3 degrees
+
+j = 2.22/1000
+# = .00222 degrees
+k=60*.00222
+l=60/13.32
+# answer = 8 seconds
+
+# in .202 degrees
+
+m=60*.202
+# 12.12 
+# = 12 minutes
+l=60/12
+# answer = 12 minutes 7.2 sec
+#%%
+# convert angles from deg-sec-min to decimal degrees
+
+# 12 degrees 15 minutes
+# 12.25 (as 15 = 25% of 60)
+
+# 34 degrees 50 minutes
+n=50/60
+
+# 34.833 (repeating)
+
+# 4 seconds
+
+o=4/3600
+# .00111 or 1.11 x 10(-3rd)
+
+# 5 deg 14 min 4.8 sec
+
+p=14/60
+q=4.8/360
+r=.233333+.0013333
+
+# 5.235
+#%%
+
+
+
+
